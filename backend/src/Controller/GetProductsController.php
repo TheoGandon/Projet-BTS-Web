65a7419a6ee3725a4743355fe976cd87c00252ff
FskiTeo <teo.fiminski@epsi.fr>
2023-12-11 16:39:02 +0100

    Fixtures & API Calls

--- backend/src/Controller/GetProductsController.php
+++ backend/src/Controller/GetProductsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticlePictureRepository;
 use App\Repository\ArticlesRepository;
+use App\Repository\ColorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,22 +14,91 @@ use function PHPUnit\Framework\isNull;
 class GetProductsController extends AbstractController
 {
     #[Route('/api/get/products', name: 'app_get_products')]
-    public function getProducts(ArticlesRepository $articlesRepository): Response
+    public function getProducts(ArticlesRepository $articlesRepository, ArticlePictureRepository $articlePictureRepository, ColorRepository $colorRepository): Response
     {
         $articles = $articlesRepository->findAll();
 
         $data = [];
 
         foreach ($articles as $article){
+            $id = $article->getId();
+            $title = $article->getArticleTitle();
+            $description = $article->getArticleDescription();
+            $selling_price = $article->getArticleSellingPrice();
+            $selling_price_promo = $article->getArticleSellingPricePromotion();
+            $pictures_objects = $articlePictureRepository->findBy(["article_id"=>$id], );
+            $pictures = array_map(function($picture) {
+                return $picture->getPictureLink();
+            }, $pictures_objects);
+
             $data[] = [
-                'id' => $article->getId(),
-                'title' => $article->getArticlTitle(),
-                'decription' => $article->getArticleDescription(),
-                'selling_price' => $article->getArticleSellingPrice(),
-                'selling_price_promo' => $article->getArticleSellingPricePromotion()
+                'id' => $id,
+                'title' => $title,
+                'description' => $description,
+                'selling_price' => $selling_price,
+                'selling_price_promo' => $selling_price_promo,
+                'pictures' => $pictures
             ];
         }
 
         return new JsonResponse($data);
+    }
+
+    #[Route('/api/get/products/by/color/{color}', name: 'app_get_products_by_color')]
+    public function getProductsByColor(string $color, ArticlesRepository $articlesRepository, ArticlePictureRepository $articlePictureRepository, ColorRepository $colorRepository): Response
+    {
+        $articles = $articlesRepository->findAll();
+
+        $data = [];
+
+        foreach ($articles as $article){
+            $id = $article->getId();
+            $title = $article->getArticleTitle();
+            $description = $article->getArticleDescription();
+            $selling_price = $article->getArticleSellingPrice();
+            $selling_price_promo = $article->getArticleSellingPricePromotion();
+            $pictures_objects = $articlePictureRepository->findBy(["article_id"=>$id], );
+            $pictures = array_map(function($picture) {
+                return $picture->getPictureLink();
+            }, $pictures_objects);
+
+            $data[] = [
+                'id' => $id,
+                'title' => $title,
+                'description' => $description,
+                'selling_price' => $selling_price,
+                'selling_price_promo' => $selling_price_promo,
+                'pictures' => $pictures
+            ];
+        }
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/api/get/product/{id}', name: 'app_get_single_product')]
+    public function getSingleProduct(int $id, ArticlesRepository $articlesRepository, ArticlePictureRepository $articlePictureRepository, ColorRepository $colorRepository): Response
+    {
+        $article = $articlesRepository->find($id);
+
+        $id = $article->getId();
+        $title = $article->getArticleTitle();
+        $description = $article->getArticleDescription();
+        $selling_price = $article->getArticleSellingPrice();
+        $selling_price_promo = $article->getArticleSellingPricePromotion();
+        $pictures_objects = $articlePictureRepository->findBy(["article_id"=>$id], );
+        $pictures = array_map(function($picture) {
+            return $picture->getPictureLink();
+        }, $pictures_objects);
+
+        $response = [
+            'id' => $id,
+            'title' => $title,
+            'description' => $description,
+            'selling_price' => $selling_price,
+            'selling_price_promo' => $selling_price_promo,
+            'pictures' => $pictures
+        ];
+
+        return new JsonResponse($response);
     }
 }
--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,92 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ArticlePicture;
 use App\Entity\Articles;
+use App\Entity\Category;
+use App\Entity\Color;
+use App\Entity\Sizes;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+
+
+        $categories = [];
+
+        for ($i = 0; $i < 3 ; $i++) {
+            $categories[$i] = new Category();
+            $categories[$i]->setCategoryName($faker->word);
+            $manager->persist($categories[$i]);
+        }
+
+        $articles=[];
+
+        for ($i = 0; $i < 10 ; $i++) {
+            $articles[$i] = new Articles();
+            $articles[$i]->setArticleTitle($faker->text(15));
+            $articles[$i]->setArticleDescription($faker->paragraph(3));
+            $articles[$i]->setArticleSellingPrice($faker->randomFloat(2,10, 500));
+            $articles[$i]->setArticleSellingPricePromotion(0);
+            $articles[$i]->setCategoryId($categories[1]);
+
+            $manager->persist($articles[$i]);
+        }
+
+        $colors = [];
+
+        $colors[0]= new Color();
+        $colors[0]->setColorLabel("Green");
+
+        $colors[1]= new Color();
+        $colors[1]->setColorLabel("Purple");
+
+        $colors[2]= new Color();
+        $colors[2]->setColorLabel("Yellow");
+
+        foreach ($colors as $color){
+            $manager->persist($color);
+        }
+
+        $j = 0;
+
+        for($i=0; $i<10; $i++){
+            $pictures[$j] = new ArticlePicture();
+            $pictures[$j]->setPictureLink("https://picsum.photos/".rand(1000, 2000).'/'.rand(2000, 1000).'/');
+            $pictures[$j]->setColorId($colors[rand(0, 2)]);
+            $pictures[$j]->setArticleId($articles[$i]);
+
+            $manager->persist($pictures[$j]);
+
+            $j++;
+
+            $pictures[$j] = new ArticlePicture();
+            $pictures[$j]->setPictureLink("https://picsum.photos/".rand(1000, 2000).'/'.rand(2000, 1000).'/');
+            $pictures[$j]->setColorId($colors[rand(0, 2)]);
+            $pictures[$j]->setArticleId($articles[$i]);
+
+            $manager->persist($pictures[$j]);
+
+            $j++;
+        }
+
+        $sizes = [];
+
+        for($i=0; $i<5; $i++){
+            $sizes[$i] = new Sizes();
+            $sizes[$i]->setSizeLabel($faker->word);
+            $sizes[$i]->addArticleId($articles[rand(0,9)]);
+
+            $manager->persist($sizes[$i]);
+        }
+
 
         $manager->flush();
     }
