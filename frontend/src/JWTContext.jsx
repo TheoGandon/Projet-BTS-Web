5a114cf5c59326f7f87fd3064d908ae2af6ac420
TheoGandon <theo.gandon@gmail.com>
2024-05-28 23:18:29 +0200

    Update refresh token

--- frontend/src/JWTContext.jsx
+++ frontend/src/JWTContext.jsx
@@ -1,6 +1,4 @@
 import React, {createContext, useCallback, useState} from "react";
-import Cookies from "universal-cookie";
-import axios from "axios";
 
 const JWTContext = createContext("");
 
@@ -13,21 +11,16 @@ export const JWTProvider = ({ children }) => {
 
     const checkToken = useCallback(() => {
         if (jwt === null || jwt === undefined || jwt === "") {
-            const cookies = new Cookies();
-            if (cookies.get('refresh_token') !== undefined) {
-                axios.get('http://localhost:8080/api/token/refresh', {
-                    refresh_token: cookies.get('refresh_token')
-                })
-                    .then(response => response.data)
+            fetch('http://localhost:8080/api/token/refresh', {
+                credentials: 'include'
+            })
+                    .then(response => response.json())
                     .then(data => {
                         setToken(data.token);
-                        cookies.set('refresh_token', data.refresh_token, {path: '/'});
                     })
-                    .catch((error) => {
-                        cookies.remove('refresh_token')
-                        console.error(error)
-                    })
-            }
+                    .catch(() => {
+                        console.error("User not connected.");
+                    });
         }
     }, [jwt]);
 
